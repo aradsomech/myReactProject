f73755c15578b0b6f7ee65204ed0430d22986bd8
aradsomech <139678018+aradsomech@users.noreply.github.com>
2023-12-17 22:12:24 +0200

    remove unused useState

--- src/pages/CardDetailPage/CardDetailPage.jsx
+++ src/pages/CardDetailPage/CardDetailPage.jsx
@@ -41,9 +41,7 @@ const CardDetailPage = () => {
   console.log(card);
   const handleDeleteCard = (_id) => {
     console.log("_id to delete (HomePage)", _id);
-    setDataFromServer((dataFromServerCopy) =>
-      dataFromServerCopy.filter((card) => card._id != _id)
-    );
+
     // dataFromServer = dataFromServer.filter((card) => card._id != _id);
     //return true for all the cards that has id that not equal to the id we want to delete
   };
